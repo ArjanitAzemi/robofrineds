701f2f7375e20928e8eaad809e6f8276c001ade6
Arjanit Azemi <az.arjanit@gmail.com>
2020-01-23 23:33:18 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,29 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Arjanit Azemi',
+    username: 'Niti',
+    email: 'az.arjanit@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Esra Junuzi',
+    username: 'Eca',
+    email: 'esra.junuzi5@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'aaab',
+    username: 'Djellza',
+    email: 'aaab@live.com'
+  },
+  {
+    id: 4,
+    name: 'cbcaa',
+    username: 'cbcaa',
+    email: 'cbcaa@gmail.com'
+  },
+
+  
+  
+];
